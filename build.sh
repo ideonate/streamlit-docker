3e5fa6498972b80d115085a1127a01ef62179130
Dan Lester <dan@danlester.com>
2020-04-18 12:05:29 +0100

    Fixed single - now checks only for py scripts!

--- build.sh
+++ build.sh
@@ -2,17 +2,20 @@
 
 cd "${0%/*}"
 
+dt=`date '+%Y%m%d%H%M%S'`
+
 cd streamlit-base
 
-docker build -t ideonate/streamlit-base .
-
+docker build -t ideonate/streamlit-base:$dt .
+docker tag ideonate/streamlit-base:$dt ideonate/streamlit-base:latest
 
 cd ../streamlit-single
 
-docker build -t ideonate/streamlit-single .
-
+docker build -t ideonate/streamlit-single:$dt .
+docker tag ideonate/streamlit-single:$dt ideonate/streamlit-single:latest
 
 
 cd ../streamlit-launchpad
 
-docker build -t ideonate/streamlit-launchpad .
+docker build -t ideonate/streamlit-launchpad:$dt .
+docker tag ideonate/streamlit-launchpad:$dt ideonate/streamlit-launchpad:latest
