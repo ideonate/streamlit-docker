768302fd02a1e0ac53ff8f2cde08eca7d350dcbd
Dan Lester <dan@danlester.com>
2020-04-30 15:43:25 +0100

    Added scipy images

--- build.sh
+++ build.sh
@@ -20,3 +20,20 @@ cd ../streamlit-launchpad
 docker build -t ideonate/streamlit-launchpad:$dt .
 docker tag ideonate/streamlit-launchpad:$dt ideonate/streamlit-launchpad:latest
 
+
+cd ../streamlit-base-scipy
+
+docker build -t ideonate/streamlit-base-scipy:$dt .
+docker tag ideonate/streamlit-base-scipy:$dt ideonate/streamlit-base-scipy:latest
+
+
+cd ../streamlit-single
+
+docker build --build-arg BASE_REPO=ideonate/streamlit-base-scipy:$dt -t ideonate/streamlit-single-scipy:$dt .
+docker tag ideonate/streamlit-single-scipy:$dt ideonate/streamlit-single-scipy:latest
+
+
+cd ../streamlit-launchpad
+
+docker build --build-arg BASE_REPO=ideonate/streamlit-base-scipy:$dt -t ideonate/streamlit-launchpad-scipy:$dt .
+docker tag ideonate/streamlit-launchpad-scipy:$dt ideonate/streamlit-launchpad-scipy:latest
