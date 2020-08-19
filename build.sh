4525b8a263c4705d228979ade29684fb99196b49
Dan Lester <dan@danlester.com>
2020-08-19 12:26:56 +0100

    Python 3.8.5, more robust Jovyan base image

--- build.sh
+++ build.sh
@@ -4,21 +4,17 @@ cd "${0%/*}"
 
 dt=`date '+%Y%m%d%H%M%S'`
 
+
 cd streamlit-base
 
-docker build -t ideonate/streamlit-base:$dt .
+docker build --build-arg PYTHON_VERSION=3.8.5 -t ideonate/streamlit-base:$dt .
 docker tag ideonate/streamlit-base:$dt ideonate/streamlit-base:latest
+
 
 cd ../streamlit-single
 
 docker build -t ideonate/streamlit-single:$dt .
 docker tag ideonate/streamlit-single:$dt ideonate/streamlit-single:latest
-
-
-cd ../streamlit-launchpad
-
-docker build -t ideonate/streamlit-launchpad:$dt .
-docker tag ideonate/streamlit-launchpad:$dt ideonate/streamlit-launchpad:latest
 
 
 cd ../streamlit-base-scipy
@@ -31,6 +27,12 @@ cd ../streamlit-single
 
 docker build --build-arg BASE_REPO=ideonate/streamlit-base-scipy:$dt -t ideonate/streamlit-single-scipy:$dt .
 docker tag ideonate/streamlit-single-scipy:$dt ideonate/streamlit-single-scipy:latest
+
+
+cd ../streamlit-launchpad
+
+docker build -t ideonate/streamlit-launchpad:$dt .
+docker tag ideonate/streamlit-launchpad:$dt ideonate/streamlit-launchpad:latest
 
 
 cd ../streamlit-launchpad
--- streamlit-single/streamlit-run-first.sh
+++ streamlit-single/streamlit-run-first.sh
@@ -7,5 +7,5 @@ then
       echo "There are no .py scripts in /app so cannot run any"
 else
       echo "Running $appname"
-      streamlit run --server.port 8888 --server.headless True --server.runOnSave True --server.enableCORS False "$appname"
+      streamlit run --server.port 8888 --server.headless true --server.runOnSave true --server.enableCORS false --server.enableXsrfProtection=false "$appname"
 fi
